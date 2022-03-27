1e5f6715a42e3938bd4027438ad9b512976b8fc5
sbx_user1051 <sbx_user1051@169.254.25.245>
2022-03-27 20:28:47 +0000

    Sun Mar 27 20:28:47 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -125,11 +125,11 @@ def compute_price(list_items, price_dict):
 
                 qty_available = qty
                 for offer_qty in qty_list_ordered:
- 
-                    if qty_available < offer_qty:
-                        print("yes")
-                        total_val += qty_available * price
-                    else:
+                    # if qty_available < offer_qty:
+                    #     print("yes")
+                    #     total_val += qty_available * price
+                    # else:
+                    if qty_available >= offer_qty:
                         eff_offer = get_offer_based_on_quantity(offers, offer_qty)
                         offer_multiple = qty_available // offer_qty
                         total_val += offer_multiple * eff_offer["deal_price"]
@@ -137,7 +137,9 @@ def compute_price(list_items, price_dict):
                         print(eff_offer["deal_price"], offer_qty)
                         
                         offer_remainder = qty_available % offer_qty
-                        qty_available = offer_remainder             
+                        qty_available = offer_remainder
+                
+                total_val += qty_available * price
 
     return total_val
 
