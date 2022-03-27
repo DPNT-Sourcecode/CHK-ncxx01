add18de0ac85710b8513ff930bddad9d77b9e2aa
sbx_user1051 <sbx_user1051@169.254.182.81>
2022-03-27 13:53:31 +0000

    Sun Mar 27 13:53:31 UTC 2022

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,15 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
+
 def compute(x, y):
-    raise NotImplementedError()
+    """Compute sum of passed in arguments
+
+    :param x: Integer (operand 1)
+    :param y: Integer (operand 2)
+
+    :rtype: Integer
+    """
+    if x > 100 or x < 0 or y > 100 or y < 0:
+        raise ValueError
+
+    return x + y
+
--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -2,5 +2,14 @@ from solutions.SUM import sum_solution
 
 
 class TestSum():
+    """Class to test sum_solution"""
+
     def test_sum(self):
+        """Happy path test"""
         assert sum_solution.compute(1, 2) == 3
+
+    # def test_sum(self):
+    #     """Happy path test"""
+    #     with self.assert_
+    #     assert sum_solution.compute(1, 2) == 3
+
